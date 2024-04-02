85b7e7faa7c7d823fbbf68d604e7ebb2752246a4
AnshSonone <sononeansh@gmail.com>
2024-04-02 19:48:49 +0530

    Updated Navbar added classname

---
+++ app/Issues/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const issuesPage = () => {
+  return (
+    <div>
+      issuesPage
+    </div>
+  )
+}
+
+export default issuesPage
+issuesPage
--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,18 +1,33 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
-import { link } from 'fs'
+import { usePathname } from 'next/navigation'
+import classNames from 'classnames'
 
 const Navbar = () => {
+    const currentPath = usePathname();
+
     const links = [
         {label:'Dashboard', href:"/"},
-        {label:'Issues', href:"/issue"}
+        {label:'Issues', href:"/Issues"}
     ]
 
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
       <Link href="/" className='font-bold'>LOGO</Link>
         <ul className='flex space-x-6'>
-            {links.map(link => <Link key={link.href} href={link.href} className='text-zinc-300 hover:text-zinc-800 transition-colors'>{link.label}</Link>)}
+            {links.map(link => 
+            <Link 
+                key={link.href} 
+                href={link.href} 
+                className={classNames({
+                    "text-zinc-300" : link.href === currentPath,
+                    "text-zinc-500" : link.href !== currentPath,
+                    "hover:text-zinc-800 transition-colors": true
+                })}
+            >
+            {link.label}
+            </Link>)}
         </ul>
     </nav>
   )
